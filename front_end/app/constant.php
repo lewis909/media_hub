8fa843c553dbfcfc988b8965279256b7033379e5
lewis909 <lewis.dt@gmail.com>
2017-01-29 08:36:33 +0000

    Update constant.php

--- front_end/app/constant.php
+++ front_end/app/constant.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_SERVER', "localhost");
-define('DB_USER', "lewis_transcode");
-define('DB_PASSWORD', "tool4602");
-define('DB_DATABASE', "media_hub");
+define('DB_USER', "username");
+define('DB_PASSWORD', "password");
+define('DB_DATABASE', "databasename");
 define('DB_DRIVER', "mysql");
